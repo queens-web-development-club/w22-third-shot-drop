ea8f663dc0e628865de8325f9eaeac890788c3b9
Maninderpal Badhan <mbadhan0202@gmail.com>
2022-02-03 20:39:27 -0500

    Create a simple UI for the sign up form

---
+++ src/SignUpForm.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import {
+	Flex,
+	Box,
+	Heading,
+	FormControl,
+	FormLabel,
+	Input,
+	Button,
+	NumberInput,
+	NumberInputField,
+	NumberInputStepper,
+	NumberIncrementStepper,
+	NumberDecrementStepper,
+  } from '@chakra-ui/react';
+
+const SignUpForm = () => {
+  return (
+	<Flex width="full" align="center" justifyContent="center">
+      <Box p={2}>
+        <Box textAlign="left">
+          <Heading>Clinic Reservations</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
+          <form>
+            <FormControl>
+              <FormLabel>First Name</FormLabel>
+              <Input type="email" placeholder="John" />
+            </FormControl>
+			<FormControl>
+              <FormLabel>Last Name</FormLabel>
+              <Input type="email" placeholder="Smith" />
+            </FormControl>
+			<FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" placeholder="name@email.com" />
+            </FormControl>
+            <FormControl mt={6}>
+              <FormLabel>Number of Additional Guests</FormLabel>
+			  <NumberInput defaultValue={0} min={0}>
+				<NumberInputField/>
+				<NumberInputStepper>
+					<NumberIncrementStepper />
+					<NumberDecrementStepper />
+				</NumberInputStepper>
+				</NumberInput>
+            </FormControl>
+            <Button width="full" mt={4} type="submit">
+              Submit
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
+  );
+};
+
+export default SignUpForm;
